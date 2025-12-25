d0c5068b7588f832424f3a001bc82fda46170602
Harris Ryder <ryderharris@icloud.com>
2025-12-25 14:17:53 +0000

    Add Arial font files and update global styles for custom font usage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid h-screen w-screen">
-      <div className="col-start-1 row-start-1 h-full w-full flex flex-col justify-between">
+    <div className="grid h-screen w-screen font-arial font-bold">
+      <div className="col-start-1 row-start-1 h-full w-full flex flex-col justify-between p-8">
         <div className="flex justify-between">
           <Link href="/">LETI RYDER</Link>
           <Link href="/">CV</Link>

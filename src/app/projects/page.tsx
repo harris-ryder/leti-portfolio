36a961f1fd1241551fc81d474e537c2ebc0f66b7
Harris Ryder <ryderharris@icloud.com>
2025-12-25 23:43:21 +0000

    fix

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,8 +5,9 @@ import { PROJECTS } from "./data/projects";
 export default function Projects() {
   return (
     <NavigationLayout>
-      <div className="w-full h-full min-h-0 overflow-y-auto p-6 px-10 md:flex md:justify-center md:items-center">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+      <div className="w-full h-full min-h-0 overflow-y-auto p-6 px-10">
+        <div className="min-h-full flex justify-center items-center">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
           {PROJECTS.map((project, index) => (
             <div
               key={index}
@@ -26,6 +27,7 @@ export default function Projects() {
               </p>
             </div>
           ))}
+          </div>
         </div>
       </div>
     </NavigationLayout>

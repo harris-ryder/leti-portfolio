5a48f1762c56fc41930f54f20675e76109597b49
Harris Ryder <ryderharris@icloud.com>
2025-12-25 19:52:42 +0000

    add timer to gallery

--- src/app/components/image-gallery.tsx
+++ src/app/components/image-gallery.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import ImageViewer, { ImageViewerProps } from "./image-viewer";
 import { cn } from "../utils";
 
@@ -10,13 +10,40 @@ interface ImageGalleryProps {
 
 export default function ImageGallery({ images }: ImageGalleryProps) {
   const [projectIndex, setProjectIndex] = useState<number>(0);
+  const timerRef = useRef<NodeJS.Timeout | null>(null);
+
+  const goToNextImage = () => {
+    setProjectIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const handleClick = () => {
+    // Clear existing timer
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+    }
+    
+    // Go to next image
+    goToNextImage();
+    
+    // Restart timer
+    timerRef.current = setTimeout(goToNextImage, 3000);
+  };
+
+  // Set up auto-advance timer
+  useEffect(() => {
+    timerRef.current = setTimeout(goToNextImage, 3000);
+
+    return () => {
+      if (timerRef.current) {
+        clearTimeout(timerRef.current);
+      }
+    };
+  }, [projectIndex, images.length]);
 
   return (
     <div
       className="w-full h-full col-start-1 row-start-1 place-self-center cursor-pointer relative bg-white"
-      onClick={() =>
-        setProjectIndex((prevIndex) => (prevIndex + 1) % images.length)
-      }
+      onClick={handleClick}
     >
       {images.map((image, index) => (
         <div

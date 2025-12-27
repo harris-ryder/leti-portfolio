21a4837f4221040dac6b66e45d5cddfbaa4451c8
Harris Ryder <ryderharris@icloud.com>
2025-12-27 16:27:02 +0000

    update other-work types structure

--- src/app/data/other-work/other-work.ts
+++ src/app/data/other-work/other-work.ts
@@ -1,22 +1,63 @@
-export const OTHER_WORK_CATEGORIES = [
+export interface OtherWorkImage {
+  id: string;
+  src: string;
+  description: string;
+}
+
+export interface OtherWork {
+  id: string;
+  description: string;
+  src: string;
+  work: Array<OtherWorkImage>;
+}
+
+export const OTHER_WORK_CATEGORIES: Array<OtherWork> = [
   {
     id: "collages",
     description: "Collages",
     src: "https://6rsxeq3ze0cbeqvf.public.blob.vercel-storage.com/other-work/collages/1.webp",
+    work: [
+      {
+        id: "collages-1",
+        src: "https://6rsxeq3ze0cbeqvf.public.blob.vercel-storage.com/other-work/collages/1.webp",
+        description: "Collage 1",
+      },
+    ],
   },
   {
     id: "drawing-and-painting",
     description: "Drawing and Painting",
     src: "https://6rsxeq3ze0cbeqvf.public.blob.vercel-storage.com/other-work/drawing-and-painting/1.webp",
+    work: [
+      {
+        id: "drawing-and-painting-1",
+        src: "https://6rsxeq3ze0cbeqvf.public.blob.vercel-storage.com/other-work/drawing-and-painting/1.webp",
+        description: "Drawing and Painting 1",
+      },
+    ],
   },
   {
     id: "photography",
     description: "Photography",
     src: "https://6rsxeq3ze0cbeqvf.public.blob.vercel-storage.com/other-work/photography/1.webp",
+    work: [
+      {
+        id: "drawing-and-painting-1",
+        src: "https://6rsxeq3ze0cbeqvf.public.blob.vercel-storage.com/other-work/drawing-and-painting/1.webp",
+        description: "Drawing and Painting 1",
+      },
+    ],
   },
   {
     id: "writing",
     description: "Writing",
     src: "https://6rsxeq3ze0cbeqvf.public.blob.vercel-storage.com/other-work/writing/1.webp",
+    work: [
+      {
+        id: "drawing-and-painting-1",
+        src: "https://6rsxeq3ze0cbeqvf.public.blob.vercel-storage.com/other-work/drawing-and-painting/1.webp",
+        description: "Drawing and Painting 1",
+      },
+    ],
   },
 ];

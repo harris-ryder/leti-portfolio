29ff024966ba6d2dd124bec7c47ac7f00c03ae46
Harris Ryder <ryderharris@icloud.com>
2025-12-27 23:28:27 +0000

    fix

--- src/app/data/projects/birthing-house-for-commune.ts
+++ src/app/data/projects/birthing-house-for-commune.ts
@@ -19,36 +19,58 @@ export const birthinHouseForCommuneArticle: ProjectArticle = [
   },
   {
     title: "Commune Birthing House",
+    newLine: false,
+  },
+  {
+    text: ` 
+Date: Spring 2025
+Instructor: Michael Meredith
+Site: Kelmscott Manor, Cotswolds, UK.
+Description 1: Until the mid-19th century, towns relied on local time, set by the sun. It didn’t matter that places at different longitudes kept different times. When railways were established, it was suddenly essential to have common time for scheduling and avoiding collisions.
+`,
+    newLine: false,
+  },
+  {
+    text: ` 
+In 1884, the International Meridian Conference was held in Washington DC, here the conference agreed to divide the world into 24-hour time zones based on the Greenwich meridian (‘railway time’). Around this time, William Morris and his contemporaries sought with the Arts and Crafts movement to oppose industrialization, instead advocating for a socialist anarchism.
+`,
+    newLine: false,
+  },
+  {
+    text: ` 
+This Commune, growing in the garden of Morris’ home Kelmscott Manor, rejects the 24 hour clock, the typical 9-5 workday, the commodification of labour, the notion of productivity. 
+`,
     newLine: true,
   },
   {
     src: "https://6rsxeq3ze0cbeqvf.public.blob.vercel-storage.com/projects/birthing-house-for-commune/7.webp",
-    description: "Initial collage for living pod shape and entrance, 1’=1/8",
+    description:
+      "The layout of the Commune, drawn with Dominica Kusmierczyk, 1’=1/32",
     fit: "cover",
     columnWidth: 900,
     newLine: true,
   },
   {
     src: "https://6rsxeq3ze0cbeqvf.public.blob.vercel-storage.com/projects/birthing-house-for-commune/3.webp",
-    description: "Initial collage for living pod shape and entrance, 1’=1/8",
+    description: "Axonometric of the Birthing House, 1’=3/16",
     fit: "cover",
     newLine: false,
   },
   {
     src: "https://6rsxeq3ze0cbeqvf.public.blob.vercel-storage.com/projects/birthing-house-for-commune/1.webp",
-    description: "Initial collage for living pod shape and entrance, 1’=1/8",
+    description: "Plan, 1’=3/16",
     fit: "cover",
     newLine: true,
   },
   {
     src: "https://6rsxeq3ze0cbeqvf.public.blob.vercel-storage.com/projects/birthing-house-for-commune/8.webp",
-    description: "Initial collage for living pod shape and entrance, 1’=1/8",
+    description: "Sketch of section, roof study, 1’=3/16",
     fit: "cover",
     newLine: true,
   },
   {
     src: "https://6rsxeq3ze0cbeqvf.public.blob.vercel-storage.com/projects/birthing-house-for-commune/10.webp",
-    description: "Initial collage for living pod shape and entrance, 1’=1/8",
+    description: "Exploded Axonometric, 1’=1/16",
     fit: "cover",
     newLine: false,
   },
@@ -62,6 +84,7 @@ export const birthinHouseForCommuneArticle: ProjectArticle = [
     src: "https://6rsxeq3ze0cbeqvf.public.blob.vercel-storage.com/projects/birthing-house-for-commune/4.webp",
     description: "Initial collage for living pod shape and entrance, 1’=1/8",
     fit: "cover",
+    columnWidth: 300,
     newLine: false,
   },
   {

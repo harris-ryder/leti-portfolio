38bd6dca280d43c4b5e44c65b328ea6420c83d66
Harris Ryder <ryderharris@icloud.com>
2025-12-30 11:13:37 +0000

    at work, photography, collages dates added

--- src/app/data/other-work/collages.ts
+++ src/app/data/other-work/collages.ts
@@ -5,24 +5,24 @@ export const COLLAGES: Array<OtherWorkImage> = [
     id: "superstudio-forgery",
     src: "https://6rsxeq3ze0cbeqvf.public.blob.vercel-storage.com/other-work/collages/1.webp",
     description: "Superstudio Forgery",
-    date: "2025",
+    date: "May 2025",
   },
   {
     id: "a-drawing-tool-a-letter-a-figure",
     src: "https://6rsxeq3ze0cbeqvf.public.blob.vercel-storage.com/other-work/collages/2.webp",
     description: "A Drawing Tool, A Letter, A Figure",
-    date: "2025",
+    date: "February 2025",
   },
   {
     id: "timed-collage",
     src: "https://6rsxeq3ze0cbeqvf.public.blob.vercel-storage.com/other-work/collages/3.webp",
     description: "Timed Collage",
-    date: "2025",
+    date: "February 2025",
   },
   {
     id: "timed-collage-2",
     src: "https://6rsxeq3ze0cbeqvf.public.blob.vercel-storage.com/other-work/collages/4.webp",
     description: "Timed Collage II",
-    date: "2025",
+    date: "March 2025",
   },
 ];

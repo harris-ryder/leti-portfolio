71ae6c3e0475ccd7c3fb42bb0e928b52887fc6cc
Harris Ryder <ryderharris@icloud.com>
2025-12-30 11:17:49 +0000

    update writing

--- src/app/data/other-work/writing/notes-on-canal-st.ts
+++ src/app/data/other-work/writing/notes-on-canal-st.ts
@@ -1,17 +1,23 @@
 export const notesOnCanalSt = `
-<p>Notes on Canal St</p>
+<p>Where Dollars Go,</p>
+<p>After Reading Benjamin's One-way Street</p>
+
 <p>19 September 2025</p>
  
+<p>www.CanalAlarm.com</p>
+ 
+<p>I read Canal Street from left to right. Each edifice, pole, car and object is labelled. The internet is there, too, but only as '.com', and undoubtedly it is there because it is everywhere. A sign for a nonphysical place is strange. The sign tells you where to go but gives no direction. The instruction is the destination. I took the trip there on my computer and when I arrived it was a map, with a further series of signs: shop, quote, contact, about, FAQs, blog…</p>
+ 
 <p>GO WHERE DOLLARS WONT. BUY, SELL & STORE CRYPTO. GEMINI.COM</p>
-
+ 
 <p>Buy</p>
-<p>Spending money isn’t the same as buying. I didn’t buy my subway ride, money was taken out of my account. Like a tax. I did buy a cup of tea. It pained me to spend $4.50 on water, but I wanted to sit in a cafe. The cost of sitting.</p>
+<p>Spending money isn't the same as buying. I didn't buy my subway ride, money was taken out of my account. Like a tax. I did buy a cup of tea. It pained me to spend $4.50 on hot water, but I wanted to sit in a cafe. The cost of sitting.</p>
  
 <p>Sell</p>
-<p>Everything in the city is for sale. I read in a business book that I am for sale too. The way I dress, my handshake, my writing.</p>
+<p>Everything in the city is for sale. I read in a business book that I am for sale, too. The way I dress, my handshake, my writing… An opening sentence is a handshake.</p>
  
 <p>Store</p>
-<p>Some of my stuff is on me, almost always: my phone, my wallet, my keys, my lip balm, my in-case-of-emergency medicines, my lighter. When I go home it sits on the chair next to me, among my other stuff: my furniture, my clothes, my shoes, my toiletries, my kitchenware, my food, my rubbish. In Cambridge another collection of belongings, enclosed by the walls of my childhood bedroom: my books, my letters, my photos, my less desirable and unseasonal clothes, my things that I love but ultimately are a bit too heavy to bring along. There is also my stuff that I have lent, given, had stolen and lost.</p>
+<p>Some of my stuff is almost always on me: my phone, my wallet, my keys, my lip balm, my in-case-of-emergency medicines, my lighter. When I go home it all sits on the chair next to me, among my other stuff: my furniture, my clothes, my shoes, my toiletries, my kitchenware, my food, my rubbish. In Cambridge is another collection of belongings, enclosed by the walls of my childhood bedroom: my books, my letters, my photos, my less desirable and unseasonal clothes, my things that I love but ultimately are a bit too heavy to bring along. There is also the stuff that I have lent, given, had stolen and lost.</p>
  
-<p>And then there is my storage in the cloud. There is everything and nothing. There somewhere are my greatest celebrations, my most earnest apologies, my impulsive thoughts, my body documented – inside my swollen throat, my spots, my grazed knee, the history of my music taste, my job offers and my rejections, my drawings, my writing, many of the meals I have eaten, my desires, my obsessions, my identification documents, all my money.</p>
+<p>And then there is my storage in the cloud. There, somewhere, are my greatest celebrations, my most earnest apologies, my impulsive thoughts, my body documented - inside my swollen throat, my spots, my grazed knee - the history of my music taste, my job offers and my rejections, my drawings, my writing, many of the meals I have eaten, my desires, my obsessions, my identification documents, and all my money.</p>
 `;
